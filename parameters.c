60b33731ce33bf424ec79a9960b36a3deb685642
Pieta9221 <101437355+Pieta9221@users.noreply.github.com>
2022-05-21 23:28:02 +0100

    Create parameters.c

---
+++ parameters.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * init_params - reset buffer and clear struct fields
+ * @params: struct of the parameters 
+ * @ap: pointer to the argument
+ *
+ * Return: void
+ */
+void init_params(params_t *params, va_list ap)
+{
+	params->unsign = 0;
+
+	params->add_flag = 0;
+	params->empty_flag = 0;
+	params->hash_flag = 0;
+	params->null_flag = 0;
+	params->not_flag = 0;
+
+	params->width = 0;
+	params->precision = UINT_MAX;
+
+	params->h_mod = 0;
+	params->l_mod = 0;
+	(void)ap;
+}
