4b41052274a2375a9d7518ebe03a9b6a02549808
Pieta9221 <101437355+Pieta9221@users.noreply.github.com>
2022-05-21 23:40:39 +0100

    Create string.c

---
+++ string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * get_precision - gets the precision from the string format
+ * @p: the string format
+ * @params: the parameters structure
+ * @ap: the pointer to the argument
+ *
+ * Return: new pointer
+ */
+char *precision(char *p, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*p != '.')
+		return (p);
+	p++;
+	if (*p == '*')
+	{
+		d = va_arg(ap, int);
+		p++;
+	}
+	else
+	{
+		while (_isdigit(*p))
+			d = d * 10 + (*p++ - '0');
+	}
+	params->precision = d;
+	return (p);
+}
