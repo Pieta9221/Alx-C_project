3853d53ef9e871cc6480190a421589ccae291c6d
layorcodes <99360584+layorcodes@users.noreply.github.com>
2022-05-23 00:11:06 +0800

    updated simple_print.c file

--- simple_print.c
+++ simple_print.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_to_fro - prints a arange of char address.
- * @start: starting address.
- * @stop: stop address.
- * @except: except address.
+ * print_from_to - prints a range of char addresses
+ * @start: starting address
+ * @stop: stopping address
+ * @except: except address
  *
  * Return: number bytes printed
  */
-int print_to_fro(char *start, char *stop, char *except)
+int print_from_to(char *start, char *stop, char *except)
 {
 	int sum = 0;
 
@@ -22,13 +22,13 @@ int print_to_fro(char *start, char *stop, char *except)
 }
 
 /**
- * print_reverse - prints string in reverse.
- * @ap: string.
- * @params: the parameters sruct.
+ * print_rev - prints string in reverse
+ * @ap: string
+ * @params: the parameters struct
  *
- * Return: number od bytes printed.
+ * Return: number bytes printed
  */
-int print_reverse(va_list ap, params_t *params)
+int print_rev(va_list ap, params_t *params)
 {
 	int len, sum = 0;
 	char *str = va_arg(ap, char *);
@@ -46,17 +46,18 @@ int print_reverse(va_list ap, params_t *params)
 }
 
 /**
- * print_rot13 - prints string in rot13.
- * @ap: string.
- * @params: the parameters struct.
+ * print_rot13 - prints string in rot13
+ * @ap: string
+ * @params: the parameters struct
  *
- * Return: number of bytes printed.
+ * Return: number bytes printed
  */
 int print_rot13(va_list ap, params_t *params)
 {
 	int i, index;
 	int count = 0;
-	char arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLM nopqrstuvwxyzabcdefghijklm";
+	char arr[] =
+		"NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
 	char *a = va_arg(ap, char *);
 	(void)params;
 
@@ -64,7 +65,8 @@ int print_rot13(va_list ap, params_t *params)
 	index = 0;
 	while (a[i])
 	{
-		if ((a[i] >= 'A' && a[i] <= 'Z') || (a[i] >= 'a' && a[i] <= 'z'))
+		if ((a[i] >= 'A' && a[i] <= 'Z')
+		    || (a[i] >= 'a' && a[i] <= 'z'))
 		{
 			index = a[i] - 65;
 			count += _putchar(arr[index]);
